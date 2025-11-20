596216f6a2a73fd3a04fcf38a855c6ee2154941c
Josevictor125 <jose7.victor7t@gmail.com>
2025-11-20 20:06:06 -0300

    Carrossel criado e imagens adicionadas

--- frontend/scr/home/home.js
+++ frontend/scr/home/home.js
@@ -1,18 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btnMenu = document.getElementById("btnMenu");
-  const menu = document.getElementById("menuLateral");
-  const overlay = document.querySelector(".overlay");
+// MENU 
+const menu = document.getElementById("menuLateral");
+const overlay = document.querySelector(".overlay");
 
-  if (btnMenu && menu && overlay) {
-    btnMenu.addEventListener("click", () => {
-      menu.classList.toggle("aberto");
-      overlay.classList.toggle("ativo");
-    });
+function alternarMenu() {
+  menu.classList.toggle("aberto");
+  overlay.classList.toggle("ativo");
+}
 
-    // fechar tocando no fundo escuro
-    overlay.addEventListener("click", () => {
-      menu.classList.remove("aberto");
-      overlay.classList.remove("ativo");
-    });
-  }
+document.getElementById("btnMenu").addEventListener("click", alternarMenu);
+overlay.addEventListener("click", alternarMenu);
+
+
+// CARROSSEL AUTOMÁTICO 
+const slides = document.querySelector(".slides");
+const totalImagens = document.querySelectorAll(".slides img").length;
+let index = 0;
+
+//  Função isolada para mover o slide (para podermos chamar ela no automático)
+function moverSlide() {
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function proximo() {
+  index++;
+  if (index >= totalImagens) index = 0;
+  moverSlide();
+}
+
+function anterior() {
+  index--;
+  if (index < 0) index = totalImagens - 1;
+  moverSlide();
+}
+
+// Cria o temporizador automático (3000ms = 3 segundos)
+let intervalo = setInterval(proximo, 3000);
+
+// Função para resetar o tempo quando o usuário clica
+function reiniciarTimer() {
+  clearInterval(intervalo); 
+  intervalo = setInterval(proximo, 3000); 
+}
+
+// Evento botão Direita
+document.querySelector(".direita").addEventListener("click", () => {
+  proximo();
+  reiniciarTimer(); 
+});
+
+// Evento botão Esquerda
+document.querySelector(".esquerda").addEventListener("click", () => {
+  anterior();
+  reiniciarTimer();
 });
