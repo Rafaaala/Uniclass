93cb417b34c5d4d7fb35248e87a72a068a6ff983
italo <italocaua19506@gmail.com>
2025-11-25 10:54:33 -0300

    Rota base / estabelecida

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('API está rodando com sucesso!');
+});
+
 app.use('/instituicoes', InstituicaoRoutes);
 
 app.listen(PORT, () => {
